ac23c932d370c771644811162e25b384097634e8
Jason Correa <jasoncorrea1987@gmail.com>
2023-02-02 20:45:36 -0500

    project 6 for tutor revised

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,4 +1,28 @@
-const formElement = document.querySelector(".form")
+enableValidation({
+  formSelector: ".popup__form",
+  inputSelector: ".popup__input",
+  submitButtonSelector: ".popup__button",
+  inactiveButtonClass: "popup__button_disabled",
+  inputErrorClass: "popup__input_type_error",
+  errorClass: "popup__error_visible"
+});
+
+function enableValidation(setting) {
+  const forms = document.querySelectorAll(setting.formSelector)
+  forms.forEach((form) => {
+    setEventListener(form, setting);
+  })
+}
+
+function setEventListener(form, setting){
+  const inputs = form.querySelectorAll(setting.inputSelector);
+  inputs.forEach((input) => {
+    input.addEventListener("input", (event) => {
+      checkInputValidity(input);
+    })
+  })
+}
+
 
 function checkInputValidity(input){
   if (input.validity.valid) {
@@ -9,13 +33,13 @@ function checkInputValidity(input){
 }
 
 function removeErrorStyles(input){
-  input.classList.remove(".input-name-error");
-  input.classList.remove(".input-type-error");
+  input.classList.remove("popup__input_type_error");
+  input.classList.remove("popup__error_visible");
 }
 
 function addErrorStyles(input){
-  input.classList.add(".input-name-error");
-  input.classList.add(".input-type-error");
+  input.classList.add("popup__input_type_error");
+  input.classList.add("popup__error_visible");
+  submitButtonSelector.classList.add("popup__button_disabled");
+  submitButtonSelector.disabled = true;
 }
-
-checkInputValidity(formElement);

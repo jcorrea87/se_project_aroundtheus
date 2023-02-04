5130a8df5694ba65797a93635b2d3a32ba508311
Jason Correa <jasoncorrea1987@gmail.com>
2023-02-03 21:53:27 -0500

    project 6 update for tutor

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,3 +1,43 @@
+const isValid = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement);
+  }else{
+    hideInputError(formElement, inputElement);
+  }
+}
+
+const showInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add("form__input_type_error");
+  errorElement.classList.add("popup__error_visible");
+}
+
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove("form__input_type_error");
+  errorElement.classList.remove("popup__error_visible");
+}
+
+const setEventListener = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll(".popup__form"));
+  inputList.forEach((inputElement) => {
+    inputElement.addEVentListener("input", () => {
+      isValid(formElement, inputElement)
+    });
+  });
+};
+
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll(".popup__form"));
+  formList.forEach((formElement) => {
+    formElement.addEventListener ("submit", (evt) => {
+      evt.preventDefault();
+    });
+
+    setEventListener(formElement);
+  });
+};
+
 enableValidation({
   formSelector: ".popup__form",
   inputSelector: ".popup__input",
@@ -6,40 +46,3 @@ enableValidation({
   inputErrorClass: "popup__input_type_error",
   errorClass: "popup__error_visible"
 });
-
-function enableValidation(setting) {
-  const forms = document.querySelectorAll(setting.formSelector)
-  forms.forEach((form) => {
-    setEventListener(form, setting);
-  })
-}
-
-function setEventListener(form, setting){
-  const inputs = form.querySelectorAll(setting.inputSelector);
-  inputs.forEach((input) => {
-    input.addEventListener("input", (event) => {
-      checkInputValidity(input);
-    })
-  })
-}
-
-
-function checkInputValidity(input){
-  if (input.validity.valid) {
-    removeErrorStyles(input);
-  }else{
-    addErrorStyles(input);
-  }
-}
-
-function removeErrorStyles(input){
-  input.classList.remove("popup__input_type_error");
-  input.classList.remove("popup__error_visible");
-}
-
-function addErrorStyles(input){
-  input.classList.add("popup__input_type_error");
-  input.classList.add("popup__error_visible");
-  submitButtonSelector.classList.add("popup__button_disabled");
-  submitButtonSelector.disabled = true;
-}
